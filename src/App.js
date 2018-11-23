75b67e82c73fbc3a564a86041db451ef13923e65
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2018-11-23 14:02:20 +0530

    commit code

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
 });
 
 const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu a`,
+  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
